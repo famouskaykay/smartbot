62a4bf1775ce4718eac8de88b322b2bb168c15f9
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-13 22:01:40 +0000

    kitchen

--- smartbot/utils/errors.py
+++ smartbot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 from smartbot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001589738293)
+LOG_GROUP_ID = int(-1001596727532)
 
 def split_limits(text):
     if len(text) < 2048:
