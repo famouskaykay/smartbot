6a55b05d8a6298608cc91c78c61c34f5ad9ec296
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-14 20:26:52 +0000

    Update __main__.py

--- smartbot/__main__.py
+++ smartbot/__main__.py
@@ -37,7 +37,7 @@ from telegram.ext import (
 from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
-from smrtbot import (
+from smartbot import (
     ALLOW_EXCL,
     BL_CHATS,
     CERT_PATH,
