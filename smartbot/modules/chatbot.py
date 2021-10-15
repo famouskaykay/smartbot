b317b2fa970eb981651dc073ae8e369214315aa1
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-15 05:12:56 +0000

    Update chatbot.py

--- smartbot/modules/chatbot.py
+++ smartbot/modules/chatbot.py
@@ -81,7 +81,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kuki-api.tk/api/Raiden/moezilla/message='+Message)
+        kukiurl = requests.get('https://kuki-api.xyz/api/Raiden/moezilla/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
