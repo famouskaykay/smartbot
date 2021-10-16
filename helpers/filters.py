c48e434b69a58656bde43d98922d890ef75506e6
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-16 20:48:11 +0000

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -2,6 +2,11 @@ from typing import Union, List
 
 from pyrogram import filters
 
+from os import name
+from pyrogram.methods import messages
+from smartbot import pbot , help_message
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
 
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
@@ -10,3 +15,10 @@ other_filters2 = filters.private & ~filters.edited & ~filters.via_bot & ~filters
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands)
+
+
+def call_back_in_filter(data):
+    return filters.create(
+        lambda flt, _, query: flt.data in query.data,
+        data=data
+    )
