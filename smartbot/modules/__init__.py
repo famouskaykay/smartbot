964a1da3146f1b6e69fd2d3868b94dc8c722a574
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-16 20:38:22 +0000

    ...

--- smartbot/modules/__init__.py
+++ smartbot/modules/__init__.py
@@ -36,7 +36,7 @@ def __list_all_modules():
         return to_load
 
     return all_modules
-
+help_message = []
 
 ALL_MODULES = __list_all_modules()
 LOGGER.info("Modules to load: %s", str(ALL_MODULES))
