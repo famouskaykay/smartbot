f640fe490809fda43158c1149852f45ddd2c1aef
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-25 18:56:39 +0000

    Create stats.py

---
+++ smartbot/modules/stats.py
@@ -0,0 +1,37 @@
+from pyrogram import filters , Client
+from smartbot import pbot as bot
+from pymongo import MongoClient 
+from smartbot import DB_URI as db_url
+
+users_db = MongoClient(db_url)['users']
+col = users_db['USER']
+
+grps = users_db['GROUPS']
+
+
+@bot.on_message(filters.command("stats"))
+def stats(_,message):
+  users = col.find({})
+  mfs = []
+  for x in users:
+    mfs.append(x['user_id'])
+    
+  total = len(mfs)
+  
+  grp = grps.find({})
+  grps_ = []
+  for x in grp:
+    grps_.append(x['chat_id'])
+    
+  total_ = len(grps_)
+  
+  bot.send_message(message.chat.id , f"Total Users: {total}\nTotal Groups: {total_}")
+  
+__help__ = """
+@famouskaykay3
+ ❍ /stats 
+
+"""
+
+
+__mod_name__ = "stats"
